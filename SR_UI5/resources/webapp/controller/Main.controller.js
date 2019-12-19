5f736e1999697343ccafc114d26f3dcae8f03be9
srameshan <shrikanth.rameshan@gmail.com>
2019-12-19 18:45:40 +0000

    CW51 Changes

---
+++ SR_UI5/resources/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("SRUI5.SR_UI5.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
